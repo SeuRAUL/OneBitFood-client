0b2f1ce7e42bd6aedf27826aa234d288e277dd04
SeuRaul <seuraul.0@gmail.com>
2021-05-12 13:04:48 -0300

    Setup & dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return 
+    <>
+      <head>
+        <title>OneBitFood V2</title>
+      </head>
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
 }
 
 export default MyApp
