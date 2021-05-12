fdc85b0315317c71fd9d6cb672e1500c75c62eef
SeuRaul <seuraul.0@gmail.com>
2021-05-12 16:32:08 -0300

    Home

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import Container from 'react-bootstrap/Container';
+import { Button, Row, Col } from 'react-bootstrap';
+import Link from 'next/link';
+import { FaCrosshairs } from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <Container>
-      Home Page :)
-    </Container>
+    <Row className="mt-8 justify-content-center">
+      <Col md={7} sx={12} className="text-center">
+        <h1 className='fw-bolder text-custom-gray-darker mb-5 lh-base display-5'>
+          Comida saudável e gostosa direto na sua casa.
+        </h1>
+
+        <Link href='/restaurants'>
+          <Button variant="custom-red" size="lg" className="text-white">
+            <FaCrosshairs className="pb-1" />
+            <spam className="px-2 fw-bolder">ENCONTRAR AGORA</spam>
+          </Button>
+        </Link>
+      </Col>
+    </Row>
   )
 }
