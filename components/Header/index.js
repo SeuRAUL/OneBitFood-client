04f6170729f58e5a1e299e1acc0ef7b2e73490dd
SeuRaul <seuraul.0@gmail.com>
2021-05-16 11:58:40 -0300

    Address

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,9 +1,14 @@
-import { Navbar } from 'react-bootstrap';
+import { useState } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
 import Image from 'next/image';
 import Link from 'next/link';
 import SearchBox from '../SearchBox';
+import AddressModal from '../AddressModal';
+import { FaCrosshairs } from 'react-icons/fa';
 
 function Header() {
+  const [ addressModalShow, setAddressModalShow ] = useState(false);
+
   return (
     <Navbar bg='white' expand='lg' className='border-bottom border-custom-gray'>
       <Navbar.Brand className='mx-3'>
@@ -21,6 +26,18 @@ function Header() {
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+        
+        <Nav className="py-2 text-center">
+        <span className="clickable-effect" onClick={() => setAddressModalShow(true)}>
+            <FaCrosshairs className="mb-1" /> Endereço
+          </span>
+          <AddressModal
+            show={addressModalShow}
+            onHide={() => setAddressModalShow(false)}
+            onShow={() => setAddressModalShow(true)}
+          />
+        </Nav>
+
         <SearchBox/>
       </Navbar.Collapse>
     </Navbar>
